18cd6fc547fe390bde840822aab7ec851b14e25e
Joseado381 <josealonzo.jaa@gmail.com>
2025-05-26 23:43:47 -0300

    en branch CalculadoraV1 se hizo un programa de calculadora

--- Program.cs
+++ Program.cs
@@ -1,25 +1,58 @@
-﻿bool esNumero;
+﻿Console.WriteLine(@"
+   ___   _   _    ___ _   _ _      _   ___   ___  ___    _   
+  / __| /_\ | |  / __| | | | |    /_\ |   \ / _ \| _ \  /_\  
+ | (__ / _ \| |_| (__| |_| | |__ / _ \| |) | (_) |   / / _ \ 
+  \___/_/ \_\____\___|\___/|____/_/ \_\___/ \___/|_|_\/_/ \_\
+");
+bool flag;
 do
 {
-    Console.WriteLine("Ingrese una cadena de numeros para ivertirlos:");
-    string texto = Console.ReadLine();
+    Console.WriteLine("\nIngrese la operacion que desea realizar: \n\t 1) Suma \n\t 2)Resta \n\t 3)Multiplicacion \n\t 4)Dividir\n\t");
 
-    int invertido = 0, aux = 0;
-
-    esNumero = int.TryParse(texto, out int numero);
-
-    if (esNumero)
+    if (int.TryParse(Console.ReadLine(), out int operacion))
     {
-        while (numero > 0)
+        flag = false;
+
+        Console.WriteLine("Ingrese dos numeros:");
+
+        bool boolA = int.TryParse(Console.ReadLine(), out int a);
+        bool boolB = int.TryParse(Console.ReadLine(), out int b);
+
+        if (boolA && boolB)
         {
-            aux = numero % 10;
-            invertido = invertido * 10 + aux;
-            numero /= 10;
+            switch (operacion)
+            {
+                case 1:
+                    int resultadoSuma = a + b;
+                    Console.WriteLine("El resultado de la suma: " + resultadoSuma);
+                    break;
+                case 2:
+                    int resultadoResta = a - b;
+                    Console.WriteLine("El resultado de la resta: " + resultadoResta);
+                    break;
+                case 3:
+                    int resultadoProducto = a * b;
+                    Console.WriteLine("El resultado del producto: " + resultadoProducto);
+                    break;
+                case 4:
+                    int resultadoDivision = a / b;
+                    int resultadoResto = a % b;
+                    Console.WriteLine("El resultado de la division: " + resultadoDivision);
+                    Console.WriteLine("El resto es: " + resultadoResto);
+                    break;
+            }
+            Console.WriteLine("Desea Realizar otro calculo? Y/N");
+            string repeticion = Console.ReadLine();
+            if (repeticion.ToUpper()=="Y")
+            {
+                flag = true;
+            }
         }
-        Console.WriteLine("El numero invertido es: " + invertido);
-    }else
-    {
-        Console.WriteLine("La cadena ingresada no es un numero");
     }
-    
-} while (esNumero == false);
+    else
+    {
+        Console.WriteLine("Ingrese una opcion valida");
+        flag = true;
+    }
+
+} while (flag);
